17c588ef72c7d91c5786b020aa1aefb0289df465
Kerem AR <keremar0000@gmail.com>
2024-03-27 21:58:45 +0300

    changed requirements for username on user.js
    changed requirements for username but will be updated on future

--- models/user.js
+++ models/user.js
@@ -9,7 +9,7 @@ const UserSchema = new Schema({
     username : {
         type: String,
         required:[true,'Email is required'],
-        match: [/^(?=.{8,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$/, "Username invalid, it should contain 8-20 alphanumeric letters and be unique!"]
+        match: [/^[a-zA-Z0-9]+$/, 'is invalid']
 
     },
     image:{
