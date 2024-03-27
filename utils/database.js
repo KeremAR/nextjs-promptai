89922359816b4106310ba4fca365e02c3c4cc639
Kerem AR <keremar0000@gmail.com>
2024-03-27 21:49:21 +0300

    password updated on database.js
    deleted < > symbols on password at database.js and received  console log but there is still an error

--- utils/database.js
+++ utils/database.js
@@ -1,22 +1,26 @@
-import mongoose from 'mongoose'
+import mongoose from 'mongoose';
 
-let isConnected = false
+let isConnected = false; // track the connection
 
 export const connectToDB = async () => {
-    mongoose.set('strictQuery', true)
-    if (isConnected) {
-        console.log('MongoDB is already connected')
-        return;
-    }
-}
-try {
+  mongoose.set('strictQuery', true);
+
+  if(isConnected) {
+    console.log('MongoDB is already connected');
+    return;
+  }
+
+  try {
     await mongoose.connect(process.env.MONGODB_URI, {
-        dbName: "share_prompt",
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
+      dbName: "share_prompt",
+      //useNewUrlParser: true,
+      //useUnifiedTopology: true,
     })
-    isConnected = true
-    console.log('MongoDB is connected')
-} catch (error) {
-    console.log(error)
+
+    isConnected = true;
+
+    console.log('MongoDB connected')
+  } catch (error) {
+    console.log(error);
+  }
 }
